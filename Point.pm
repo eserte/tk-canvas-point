2244b6f7d03388220b7738ad0fec08de2515afc0
eserte <eserte>
2002-07-24 15:24:24 +0000

    $Tk::VERSION hack (why?)

--- Point.pm
+++ Point.pm
@@ -1,7 +1,7 @@
 # -*- perl -*-
 
 #
-# $Id: Point.pm,v 1.1 2002/07/24 13:09:06 eserte Exp $
+# $Id: Point.pm,v 1.2 2002/07/24 15:24:24 eserte Exp $
 # Author: Slaven Rezic
 #
 # Copyright (C) 2002 Slaven Rezic. All rights reserved.
@@ -13,9 +13,12 @@
 #
 
 package Tk::Canvas::Point;
+use Tk;
+use Tk::Canvas;
 use strict;
 use vars qw($VERSION);
-$VERSION = sprintf("%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/);
+#$VERSION = sprintf("%d.%02d", q$Revision: 1.2 $ =~ /(\d+)\.(\d+)/);
+$VERSION = $Tk::VERSION;
 
 use base qw(DynaLoader);
 
