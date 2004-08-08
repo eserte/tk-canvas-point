a57dbdb0c2956365240f58a5760d4c1d336a659e
eserte <eserte>
2004-08-08 16:30:43 +0000

    0.02

--- Point.pm
+++ Point.pm
@@ -1,14 +1,14 @@
 # -*- perl -*-
 
 #
-# $Id: Point.pm,v 1.3 2002/07/25 08:19:48 eserte Exp $
+# $Id: Point.pm,v 1.4 2004/08/08 16:30:43 eserte Exp $
 # Author: Slaven Rezic
 #
-# Copyright (C) 2002 Slaven Rezic. All rights reserved.
+# Copyright (C) 2002,2004 Slaven Rezic. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
-# Mail: slaven.rezic@berlin.de
+# Mail: slaven@rezic.de
 # WWW:  http://www.rezic.de/eserte/
 #
 
@@ -17,7 +17,7 @@ use Tk;
 use Tk::Canvas;
 use strict;
 use vars qw($VERSION);
-$VERSION = "0.01";
+$VERSION = "0.02";
 
 use base qw(DynaLoader);
 
