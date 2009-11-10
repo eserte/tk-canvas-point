0d3d11cd0a807948b51d8397de46502469b58d0d
Slaven Rezic <slaven@rezic.de>
2009-11-10 21:06:54 +0100

    * preparing 0.06

--- Point.pm
+++ Point.pm
@@ -4,7 +4,7 @@
 # $Id: Point.pm,v 1.7 2007/11/20 21:57:50 eserte Exp $
 # Author: Slaven Rezic
 #
-# Copyright (C) 2002,2004,2007 Slaven Rezic. All rights reserved.
+# Copyright (C) 2002,2004,2007,2009 Slaven Rezic. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -17,7 +17,7 @@ use Tk;
 use Tk::Canvas;
 use strict;
 use vars qw($VERSION);
-$VERSION = "0.05";
+$VERSION = "0.06";
 
 use base qw(DynaLoader);
 
